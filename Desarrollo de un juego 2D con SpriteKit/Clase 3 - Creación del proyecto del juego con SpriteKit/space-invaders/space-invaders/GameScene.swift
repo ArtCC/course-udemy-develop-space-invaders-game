f9326448925cfb111b52322a59a85dafb662ce53
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2024-02-16 18:46:13 +0100

    Add code

---
+++ Desarrollo de un juego 2D con SpriteKit/Clase 3 - Creación del proyecto del juego con SpriteKit/space-invaders/space-invaders/GameScene.swift
@@ -0,0 +1,38 @@
+//
+//  GameScene.swift
+//  space-invaders
+//
+//  Created by Arturo Carretero Calvo on 16/2/24.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    override func update(_ currentTime: TimeInterval) {
+    }
+
+    override func didMove(to view: SKView) {
+    }
+
+    func touchDown(atPoint pos : CGPoint) {
+    }
+
+    func touchMoved(toPoint pos : CGPoint) {
+    }
+
+    func touchUp(atPoint pos : CGPoint) {
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+}
---
+++ Desarrollo de un juego 2D con SpriteKit/Clase 4 - Creación de la escena inicial del juego/space-invaders/space-invaders/GameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GameViewController.swift
+//  space-invaders
+//
+//  Created by Arturo Carretero Calvo on 16/2/24.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.view as! SKView? {
+        }
+    }
+}
