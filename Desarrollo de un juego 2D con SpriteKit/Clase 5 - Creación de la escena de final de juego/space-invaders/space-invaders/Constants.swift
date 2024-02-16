1be5e05dff64999289b75e61b321472d2ed236ca
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2024-02-16 19:21:36 +0100

    Add code

---
+++ Desarrollo de un juego 2D con SpriteKit/Clase 5 - Creación de la escena de final de juego/space-invaders/space-invaders/Constants.swift
@@ -0,0 +1,18 @@
+//
+//  Constants.swift
+//  space-invaders
+//
+//  Created by Arturo Carretero Calvo on 16/2/24.
+//
+
+import Foundation
+
+enum Constants {
+    enum Fonts {
+        static let courier = "Courier"
+    }
+
+    enum Images {
+        static let logo = "logo"
+    }
+}
